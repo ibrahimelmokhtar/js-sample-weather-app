e82280cb4a6404136fee17e3222c31e4a0fe24ce
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-01-31 15:50:08 +0200

    Setup local server

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 // Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
 
-// Start up an instance of app
+// Express to run server and routes:
+const express = require('express');
+
+// Start up an instance of app:
+const app = express();
 
 /* Dependencies */
-/* Middleware*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-//Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
+/**
+ * Middleware
+ * Here we are configuring express to use body-parser as middle-ware.
+ */
 
-// Initialize the main project folder
+// Parse a specific media type: (application/json):
+app.use(bodyParser.json());
 
-// Spin up the server
-// Callback to debug
+// Parse requests where the (Content-Type) header matches the (type) option ONLY:
+app.use(bodyParser.urlencoded({extended: 'false'}));
+
+// Cors for cross origin allowance:
+app.use(cors());
+
+// Initialize the main project folder:
+app.use(express.static('./website'));
+
+// Spin up the server:
+const port = 8080;
+const hostName = '127.0.0.1';
+const server = app.listen(port, listening);
+
+// Callback to debug:
+function listening() {
+    console.log(`server is up and running on http://${hostName}:${port}`);
+}
 
 // Initialize all route with a callback function
 
