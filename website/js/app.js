7e9421f2dfb44e82a2bf80aa695f2775ca956dac
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-01-31 17:13:17 +0200

    Add event listener for (Generate) button

--- website/js/app.js
+++ website/js/app.js
@@ -1,12 +1,22 @@
 // Personal API Key for OpenWeatherMap API
 
-// Event listener to add function to existing HTML DOM element
+// Obtain specific elements from the DOM:
+const inputZipcode = document.querySelector('#zip');
+const inputFeeling = document.querySelector('#feelings');
+const btnGenerate = document.querySelector('#generate');
 
-/* Function called by event listener */
+// Function called by the event listener of (Generate) button:
+const generateNewEntry = () => {
+    console.log('Button clicked ...');
+    console.log(inputZipcode.value);
+    console.log(inputFeeling.value);
+};
+
+// Event listener for the (Generate) button:
+btnGenerate.addEventListener('click', generateNewEntry);
 
 /* Function to GET Web API Data*/
 
 /* Function to POST data */
-
 
 /* Function to GET Project Data */
