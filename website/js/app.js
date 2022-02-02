7d59bce4ce97dbb8636ba876c167a22e9d3e82c5
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-02-02 16:04:43 +0200

    Extract specific data to displayed to the user

--- website/js/app.js
+++ website/js/app.js
@@ -1,3 +1,7 @@
+/**
+ * Start of Global Variables.
+ */
+
 // Personal API Key for OpenWeatherMap API:
 const apiToken = '95ed0460b8368f3354d9bf96c2a9b0b3';
 const apiKey = `${apiToken}&units=imperial`;
@@ -10,29 +14,80 @@ const inputZipcode = document.querySelector('#zip');
 const inputFeeling = document.querySelector('#feelings');
 const btnGenerate = document.querySelector('#generate');
 
-// Function called by the event listener of (Generate) button:
-const generateNewEntry = () => {
-    if (inputZipcode.value !== '') {
-        // get the entered zip code:
-        const zipCode = inputZipcode.value;
+/**
+ * End of Global Variables.
+ * Start of Helper Functions.
+ */
 
-        // fetch web api data:
-        try {
-            fetchWebData(baseURL, zipCode, apiKey)
-                .then(data => {
-                    console.log(data);
-                });
-        } catch (error) {
-            console.log(`error: ${error}`);
-        }
+// Capture data entered by the user from user interface.
+const captureUserData = () => {
+    // capture user inputs from the website:
+    const userData = {
+        zipCode: inputZipcode.value,
+        feeling: inputFeeling.value,
+    };
+
+    // check input is valid:
+    if (userData.zipCode !== '') {
+        // generate new entry using the entered data:
+        generateNewEntry(userData);
     }
     else {
         console.log('zip code is NOT specified ...');
     }
 };
 
-// Event listener for the (Generate) button:
-btnGenerate.addEventListener('click', generateNewEntry);
+// Extract specific parts from date object found in the Web API data.
+const extractDate = (dateString) => {
+    const date = dateString.split(' ').slice(0, 4).join(' ');
+
+    return date;
+};
+
+// Extract specific data from both user data and WEB API data.
+const extractDataToPost = (userData, webAPIData) => {
+    // extract date from Web API data, formated as: (DayName MONTH DAY YEAR):
+    const date = extractDate(Date(webAPIData.dt));
+
+    // data to be posted to the server:
+    const combinedData = {
+        tempInFahrenheit: webAPIData.main.temp,
+        date: date,
+        userResponse: userData.feeling,
+    };
+
+    return combinedData;
+};
+
+//  Display appropriate error message.
+const handleErrors = (error) => {
+    console.log(`error: ${error}`);
+};
+
+/**
+ * End of Helper Functions.
+ * Start of Main Finctions.
+ */
+
+// Function called by the event listener of (Generate) button:
+const generateNewEntry = async (userData) => {
+    // get the zipcode value:
+    const zipCode = userData.zipCode;
+
+    // fetch data from web API:
+    try {
+        fetchWebData(baseURL, zipCode, apiKey)
+        .then(webAPIData => {
+            // construct desired data object to be posted:
+            const combinedData = extractDataToPost(userData, webAPIData);
+
+            // post constructed data:
+            postData('/addEntry', combinedData);
+        });
+    } catch (error) {
+        handleErrors(error);
+    }
+};
 
 // Function to GET Web API Data:
 const fetchWebData = async (baseURL, zipCode, apiKey) => {
@@ -44,22 +99,38 @@ const fetchWebData = async (baseURL, zipCode, apiKey) => {
         const response = await fetch(url);
 
         // convert the response data into json data:
-        const data = await response.json();
+        const webAPIData = await response.json();
 
         // check if the response is done successfully:
-        if (data.cod !== 200) {
+        if (webAPIData.cod !== 200) {
             console.clear();
-            window.alert(`Code: ${data.cod}\nMessage: ${data.message}`);
+            window.alert(`Code: ${webAPIData.cod}\nMessage: ${webAPIData.message}`);
         }
         else {
-            return data;
+            return webAPIData;
         }
 
     } catch(error) {
-        console.log(`error: ${error}`);
+            handleErrors(error);
     }
 };
 
-/* Function to POST data */
+// Function to POST data:
+const postData = async (url, data) => {
+    console.log(data);
+};
 
-/* Function to GET Project Data */
+// Function to GET Project Data:
+
+
+/**
+ * End of Main Functions.
+ * Start of Event Listeners.
+ */
+
+// Event listener for the (Generate) button:
+btnGenerate.addEventListener('click', captureUserData);
+
+/**
+ * End of Event Listeners.
+ */
